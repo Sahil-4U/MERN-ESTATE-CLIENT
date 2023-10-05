a8ad306c65f25c65436b93426435eed53c13e6c7
Sahil <ss6611786@gmail.com>
2023-10-05 13:25:38 +0530

    Pages creation and routes creation done now our next part is header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
 
 
 function App() {
   return (
     <div>
-      <h1 className='text-3xl font-bold text-center'>hi react</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+
+      </Routes>
     </div>
   );
 }
