92a0e297940dcd0273bf19ffcda55caf005112f9
Sahil <ss6611786@gmail.com>
2023-10-20 12:47:07 +0530

    create some states to handle better error handling

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 
 function SignUp() {
     const [formData, setformData] = useState({});
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
     const handleChange = (e) => {
         setformData({
             ...formData,
@@ -11,15 +14,25 @@ function SignUp() {
     }
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const res = await fetch('http://localhost:6400/api/auth/sing-up', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(formData),
-        });
-        const data = await res.json();
-        alert(data);
+        try {
+            setLoading(true);
+            const res = await fetch('http://localhost:6400/api/auth/sing-up', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+            if (data.success === false) {
+                setError(data.message);
+                setLoading(false);
+                return;
+            }
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+        }
     }
     return (
         <div className='p-3 max-w-lg mx-auto'>
@@ -28,21 +41,23 @@ function SignUp() {
                 <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange} />
                 <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
                 <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
-                <button className='bg-slate-700 text-white p-3 rounded-lg uppercase  hover:opacity-80 disabled:opacity-80'>
-                    Sing up
+                <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase  hover:opacity-80 disabled:opacity-80'>
+                    {loading ? 'loading...' : 'Sign up'}
                 </button>
-                <div className='flex gap-2 mt-5'>
-                    <p>
-                        Have an account ?
-                    </p>
-                    <Link to={"/sign-in"}>
-                        <span className='text-blue-700'>
-                            SignIn
-                        </span>
-                    </Link>
-                </div>
+
 
             </form>
+            <div className='flex gap-2 mt-5'>
+                <p>
+                    Have an account ?
+                </p>
+                <Link to={"/sign-in"}>
+                    <span className='text-blue-700'>
+                        SignIn
+                    </span>
+                </Link>
+            </div>
+            {error && <p className='text-red-500 mt-5'>{error}</p>}
         </div>
     )
 }
