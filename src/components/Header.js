1f2491bb68296c70dcd7e330a8124db378117588
Sahil <ss6611786@gmail.com>
2023-11-01 11:48:02 +0530

    updating the header and created protected routes for profile section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function Header() {
+    const { currentUser } = useSelector(state => state.user);
     return (
         <header className='bg-slate-200 shadow-md'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -20,8 +22,12 @@ function Header() {
                     <Link to={'/about'}>
                         <li className='hidden sm:inline text-slate-700 hover:underline' >About</li>
                     </Link>
-                    <Link to={'/sign-up'}>
-                        <li className=' text-slate-700 hover:underline'>{' '}Signup</li>
+                    <Link to={'/profile'}>
+                        {currentUser ? (
+                            <img src={currentUser.avatar} alt='profile' className='rounded-full h-7 w-7 object-cover' />
+                        ) :
+                            <li className=' text-slate-700 hover:underline'>{' '}Sign In</li>
+                        }
                     </Link>
 
                 </ul>
@@ -30,4 +36,4 @@ function Header() {
     )
 }
 
-export default Header
+export default Header;
